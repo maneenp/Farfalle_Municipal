205d2db66c550fd26d0427e1fe333551df2627d2
Maneesha <maneesha.premasiri@gmail.com>
2025-04-15 21:07:17 +0200

    Add qf and bq in the yacy and sources in user prompts

--- src/backend/search/providers/yacy.py
+++ src/backend/search/providers/yacy.py
@@ -3,20 +3,23 @@ import urllib.parse
 import requests
 from backend.schemas import SearchResponse, SearchResult
 from backend.search.providers.base import SearchProvider
+import sys
+sys.path.append("/workspace")
+from user_prompts.user_config import SOLR_QF, SOLR_BQ, SOURCE_COUNT
 
 
 class YacySearchProvider(SearchProvider):
     def __init__(self, yacy_host, source_count):
         print("im in Yacy")
         self.yacy_host = yacy_host
-        self.source_count = source_count
+        self.source_count = SOURCE_COUNT
         urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
                 
 
     async def search(self, query: str, solr_query_type: bool = True) -> SearchResponse:
         try:
             if solr_query_type:
-                search_response = self.search_solr(query, use_keyword=False)    
+                search_response = self.search_solr(query)    
             else:
                 search_response = self.search_json(query)
 
@@ -25,12 +28,14 @@ class YacySearchProvider(SearchProvider):
         except requests.exceptions.RequestException as e:
             print("An error occurred in yacy wihtout requests:", e)
 
-    async def search_keyword(self, query_keyword: str, solr_query_type: bool = True) -> SearchResponse:
+    async def search_keyword(self, query_keyword_list: list, solr_query_type: bool = True) -> SearchResponse:
         try:
+            key_word_str = " ".join(query_keyword_list)
+            key_word_stripped = key_word_str.strip('"')
             if solr_query_type:
-                search_response = self.search_solr(query_keyword, use_keyword=True)        
+                search_response = self.search_solr(key_word_stripped)        
             else:
-               search_response = self.search_json(query_keyword)
+                search_response = self.search_json(key_word_stripped)
                
             return search_response
 
@@ -38,21 +43,8 @@ class YacySearchProvider(SearchProvider):
             print("An error occurred in yacy keyword requests:", e)
 
         
-    def search_solr(self, query: str, use_keyword: bool) -> SearchResponse:
-        if use_keyword:
-            key_word_query = " ".join(query)
-            key_word_query = key_word_query.strip('"')
-            params = {
-                    "q": key_word_query	,  # This ensures no quotes are explicitly added
-                    "defType": "edismax",
-                    "q.op": "OR",
-                    "start": 0,
-                    "rows": self.source_count,
-                    "core": "collection1",
-                    "wt": "json",
-                    }
-        else:
-            params = {
+    def search_solr(self, query: str) -> SearchResponse:
+        params = {
                  "q": query	,  # This ensures no quotes are explicitly added
                 "defType": "edismax",
                 "q.op": "OR",
@@ -60,8 +52,19 @@ class YacySearchProvider(SearchProvider):
                 "rows": self.source_count,
                 "core": "collection1",
                 "wt": "json",
+                "sort": "score desc",
+                "debugQuery":"true",
                 }
-                  # Use urlencode to safely encode the entire query
+        
+        if len(SOLR_QF):
+            qf_string = " ".join(f"{field}^{boost}" for field, boost in SOLR_QF.items() if boost is not None)
+            params["qf"] = qf_string
+
+        if len(SOLR_BQ):
+            bq_string = " ".join(f"crawldepth_i:{k}^{v}" for k, v in SOLR_BQ.items())
+            params["bq"] = bq_string
+
+        # Use urlencode to safely encode the entire query
         encoded_params = urllib.parse.urlencode(params)
                  # Construct the full URL
         request_query = f"{self.yacy_host}/solr/select?{encoded_params}"
@@ -104,4 +107,5 @@ class YacySearchProvider(SearchProvider):
 
         search_response = SearchResponse(results=results)
         return search_response
+
 
